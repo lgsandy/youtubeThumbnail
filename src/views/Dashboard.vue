57b44c6cf576e4bc698d46ab16632582be524eb9
Krishnakant Sharma <krishnakant.sharma@semasoft.co.in>
2020-04-17 15:38:35 +0530

    updated image

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,25 +1,44 @@
 <template>
   <div class="team">
-    <h1 class="subheading grey--text">Dashboard</h1>
     <v-container>
       <v-form ref="form">
-        <v-layout row wrap justify-center>
-          <v-flex sm6 xs12 md6 lg3 ma-2>
+        <v-layout row wrap>
+          <v-flex>
             <v-text-field
               :rules="searchVal"
-              v-model="uoutubeUrl"
+              v-model="youtubeUrl"
               outlined
               prepend-icon="search"
               label="Search"
             ></v-text-field>
           </v-flex>
-          <v-flex sm6 xs12 md6 lg3 ma-2>
+          <v-flex >
             <v-btn large @click="search">search</v-btn>
           </v-flex>
         </v-layout>
       </v-form>
       <v-layout row wrap justify-center>
-        <v-flex sm6 xs12 md6 lg3>dfhjds</v-flex>
+
+      <v-card class="mx-auto" max-width="400" v-for="thumb in allQualityThumb" :key="thumb.name" >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src="thumb.url"
+    >
+    </v-img>
+    <v-card-actions>
+      <a :href="thumb.url" :download="thumb.name">
+      <v-btn color="orange" text >Download
+      </v-btn>
+      </a>
+    </v-card-actions>
+     <v-card-actions>
+      <v-btn color="orange" text>{{thumb.size}}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+  
       </v-layout>
     </v-container>
   </div>
@@ -33,27 +52,24 @@ export default {
   components: {},
   data: () => ({
     searchVal: [v => !!v || "Work Order Number is required"],
-    team: [
-      { name: "Iyad", role: "web developer", avatar: "/img1.png" },
-      { name: "Reda", role: "Graphic designer", avatar: "/img2.png" },
-      { name: "Zineb", role: "web developer", avatar: "/img3.png" },
-      { name: "Hu TechGroup", role: "Desktop developer", avatar: "/img4.png" }
-    ],
-    uoutubeUrl: "",
+    allQualityThumb: [],
+    youtubeUrl: "",
     videoId: ""
   }),
   methods: {
     search() {
       if (this.$refs.form.validate()) {
-        let quality = ["mqdefault", "hqdefault", "sddefault", "maxresdefault"];
-        let obj = {
-          name: "image1",
-          url: "http://i.ytimg.com/vi/" + this.videoId + "/mqdefault.jpg"
-        };
-        this.videoId = this.youtubeUrl.split("v=")[1];
-        console.log(this.videoId);
-        console.log(obj);
-        console.log(quality);
+        this.allQualityThumb=[];
+          this.videoId = this.youtubeUrl.split("v=")[1];
+        let quality = [{name:"mqdefault",size:"MQ 320x180"},
+                       {name:"hqdefault",size:"HQ 480x360"}, 
+                       {name:"sddefault" ,size:"SD 640x480"}, 
+                        {name:"maxresdefault",size:"HD 1920x1080"}];
+        for(let i=0;i<quality.length;i++){
+        let obj = { name: `image${i}`,size:quality[i].size,url: `http://i.ytimg.com/vi/${this.videoId}/${quality[i].name}.jpg`};
+         this.allQualityThumb.push(obj);
+        }
+        console.log(this.allQualityThumb);
       }
     }
   }
