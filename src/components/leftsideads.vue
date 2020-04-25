f160a92cde71d3a4f671ad3d99f8f39de88e5289
Krishnakant Sharma <krishnakant.sharma@semasoft.co.in>
2020-04-25 17:01:46 +0530

    updated ads

---
+++ src/components/leftsideads.vue
@@ -0,0 +1,15 @@
+<template>
+   <v-img
+          src="https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg"
+        ></v-img>
+</template>
+<script>
+export default {
+  data: () => ({
+    drawer: 'adsLoading',
+  }),
+  components: {
+    // Popup
+  }
+};
+</script>
