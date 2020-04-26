991d916afdcb9624e61ba3a93266d15fb1495210
Krishnakant Sharma <krishnakant.sharma@semasoft.co.in>
2020-04-26 14:39:50 +0530

    contact us

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -4,7 +4,7 @@
       <v-row align="end" class="lightbox white--text pa-2 fill-height">
         <v-col align="center" class="justify-center">
           <div class="display-1">Contact Us</div>
-          <div class="body-1">email@gmail.com</div>
+          <div class="body-1">{{contactDetails.email}}</div>
         </v-col>
       </v-row>
     </v-img>
@@ -13,6 +13,18 @@
         <v-card-title>
           <v-icon large left>note</v-icon>
           <span class="title font-weight-light">Details</span>
+          <v-spacer />
+
+          <v-btn
+            v-if="isAdminLogin"
+            color="primary"
+            fab
+            small
+            dark
+            @click="editContactDialog = true"
+          >
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
         </v-card-title>
 
         <v-list rounded color="#26c6da" dark max-width="400">
@@ -23,7 +35,7 @@
               </v-list-item-icon>
               <v-spacer />
               <v-list-item-content>
-                <v-list-item-title>Sandy</v-list-item-title>
+                <v-list-item-title>{{contactDetails.name}}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </v-list-item-group>
@@ -34,7 +46,7 @@
               </v-list-item-icon>
               <v-spacer />
               <v-list-item-content>
-                <v-list-item-title>09883736776</v-list-item-title>
+                <v-list-item-title>{{contactDetails.phoneNo}}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </v-list-item-group>
@@ -44,48 +56,96 @@
                 <v-icon>library_books</v-icon>
               </v-list-item-icon>
               <v-spacer />
-              <v-list-item-content>Address</v-list-item-content>
+              <v-list-item-content>{{contactDetails.address}}</v-list-item-content>
             </v-list-item>
           </v-list-item-group>
         </v-list>
-        <div class="my-2" style="text-align:center" v-if="isAdminLogin">
-        <v-btn  color="primary" @click="updateAboutUs">Update</v-btn>
-       </div>
-
-        <!-- <v-card-actions>
-          <v-list-item class="grow">
-            <v-list-item-avatar color="grey darken-3">
-              <v-img
-                class="elevation-6"
-                src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-              ></v-img>
-            </v-list-item-avatar>
-
-            <v-list-item-content>
-              <v-list-item-title>Evan You</v-list-item-title>
-            </v-list-item-content>
-
-            <v-row align="center" justify="end">
-              <v-icon class="mr-1">mdi-heart</v-icon>
-              <span class="subheading mr-2">256</span>
-              <span class="mr-1">·</span>
-              <v-icon class="mr-1">mdi-share-variant</v-icon>
-              <span class="subheading">45</span>
-            </v-row>
-          </v-list-item>
-        </v-card-actions>-->
       </v-card>
     </v-row>
+    <div class="text-center">
+      <v-dialog v-model="editContactDialog" width="500" persistent>
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>Update Contact details</v-card-title>
+
+          <v-card-text>
+            <v-form ref="updateContact">
+              <v-row dense>
+                <v-col cols="12" md="12">
+                  <v-text-field
+                    v-model="contactDetails.name"
+                    :rules="nameRules"
+                    label="Name"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="12">
+                  <v-text-field
+                    v-model="contactDetails.email"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="12">
+                  <v-text-field
+                    v-model="contactDetails.phoneNo"
+                    :rules="phoneNoRules"
+                    label="Mobile No"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="12">
+                  <v-textarea
+                    rows="3"
+                    v-model="contactDetails.address"
+                    label="Address"
+                    hint="Enter Address"
+                  ></v-textarea>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="error" text @click="updateContactDetails">Update</v-btn>
+            <v-btn color="primary" text @click="editContactDialog = false">Cancel</v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { db } from "../fireBase/firebaseauth";
 export default {
   name: "projects",
   components: {},
   data: () => ({
+    editContactDialog: false,
+
+    contactDetails: {
+      name: "",
+      email: "",
+      phoneNo: "",
+      address: ""
+    },
+    nameRules: [
+      v => !!v || "Name is required",
+    ],
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+/.test(v) || "E-mail must be valid"
+    ],
+    phoneNoRules: [
+      v => !!v || "Mobile No is required",
+      v => /^[0-9_ ]*$/.test(v) || "Enter Valid Mobile No"
+    ],
     projects: [
       { title: "Site web ", person: "khaled", due: "10/10/1987", status: "no" },
       {
@@ -107,19 +167,48 @@ export default {
         status: "up"
       }
     ],
-    isAdminLogin:false
+    isAdminLogin: false
   }),
-  created(){
-  if(localStorage && localStorage.adminData && localStorage.adminData.length>0){
-    this.isAdminLogin=true;
-    }else{
-      this.isAdminLogin=false;
+  created() {
+    if (
+      localStorage &&
+      localStorage.adminData &&
+      localStorage.adminData.length > 0
+    ) {
+      this.isAdminLogin = true;
+    } else {
+      this.isAdminLogin = false;
     }
+    this.loadContactUs();
   },
   methods: {
     sortBy(prop) {
       this.projects.sort((a, b) => (a[prop] < b[prop] ? -1 : 1));
-    }
+    },
+    updateContactDetails() {
+      if (this.$refs.updateContact.validate()) {
+        console.log(this.contactDetails);
+        if (localStorage && localStorage.adminData && localStorage.adminData.length) {
+        let ref = db.collection("youtubethumb").doc("contactUs");
+        ref.set(this.contactDetails).then(res=>{
+            this.editContactDialog=false;
+         if(res){
+           console.log("update");
+         }
+        })
+      }
+      }
+    },
+loadContactUs(){
+if (localStorage && localStorage.adminData && localStorage.adminData.length) {
+        let ref = db.collection("youtubethumb").doc("contactUs");
+        ref.onSnapshot(res => {
+          if (res && res.data()) {
+            this.contactDetails=res.data();
+          }
+        });
+      }
+},
   }
 };
 </script>
